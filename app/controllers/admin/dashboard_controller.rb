dbeecb0e9e05e5f29beafbdf789f7be5f8817370
joshuavottero <joshuavottero@outlook.com>
2022-02-03 15:12:37 +0000

    change text in admin to include the total produts in the db as well as the total categories both include the link to the admin product page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,13 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["AUTHENTICATION_NAME"], password: ENV["AUTHENTICATION_PASSWORD"],
 except: []
   def show
+    @product_total = Product.all.count()
+    @category_total = Category.all.count()
+  end
+
+  private
+
+  def info_about_products()
+   
   end
 end
