9a9e37a0e9a0697005bc97f2fc49aa6012ff4f53
joshuavottero <joshuavottero@outlook.com>
2022-02-03 21:13:41 +0000

    admins can now create new categories in admin/categories new.html.erb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,31 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["AUTHENTICATION_NAME"], password: ENV["AUTHENTICATION_PASSWORD"],
+except: []
+  
   def index
     @categories = Category.all.order(:name)
   end
 
   def new
+    @categories = Category.new
+  end
 
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
