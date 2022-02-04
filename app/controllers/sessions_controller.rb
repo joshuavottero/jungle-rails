706feba31472290c9a5f4f80607ab643e2a2fc7b
joshuavottero <joshuavottero@outlook.com>
2022-02-04 02:02:04 +0000

    users can now sing up for accounts, log in to accounts and sign out of accounts. a helper method called current_user has been added to application controller that is used by the top_nav to tell if the user is sign in or not

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to :root
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,15 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to :root
     else
-      redirect_to '/signup'
+      redirect_to :back
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
